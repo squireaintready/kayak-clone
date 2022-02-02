3e79a7d79bd5889768ad304b92c2f075c5c53355
Samuel Jo <jo.sunghak@gmail.com>
2022-02-02 13:39:25 -0500

    refactor/organized code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import $ from "jquery";
-import { uuid } from "uuidv4";
+import { v4 as uuid } from "uuid";
 
 import Card from "./components/Card/Card";
 import Header from "./components/Header/Header";
 
-const baseURL = `kayak.com/h/mobileapis/directory/airlines/homework`;
-
 function App() {
+  const [data, setData] = useState([]);
+  const [checkedValues, setCheckedValues] = useState([])
+
   const getDataFromJsonP = () => {
     $.ajax({
       url: "https://www.kayak.com/h/mobileapis/directory/airlines/homework",
@@ -17,25 +18,43 @@ function App() {
       headers: { "X-Requested-With": "XMLHttpRequest" },
       crossDomain: true,
       dataType: "jsonp",
-      // }).then(res => setData(res));
     }).then((res) =>
-      setData(
-        res.filter(
-          (res) =>
-            res.alliance === "ST" ||
-            res.alliance === "OW" ||
-            res.alliance === "SA"
-        )
-      )
+      res.filter((res) => {
+        if (
+          res.alliance === "ST" ||
+          res.alliance === "OW" ||
+          res.alliance === "SA"
+        ) {
+          setData((prevState) => [
+            ...prevState,
+            {
+              logoURL: res.logoURL,
+              name: res.name,
+              alliance: res.alliance,
+              phone: res.phone,
+              site: res.site
+            },
+          ]);
+        }
+        return null;
+      })
     );
   };
+
   useEffect(() => {
     getDataFromJsonP();
-    setFilter("none");
   }, []);
 
-  const [data, setData] = useState();
-  const [filter, setFilter] = useState("");
+  
+  const handleChange = (e) =>{
+    if(checkedValues.includes(e.target.value)){
+      let temp = checkedValues.filter(value => value !== e.target.value)
+      setCheckedValues(temp)
+    }else{
+      setCheckedValues(prevState => ([...prevState, e.target.value]))
+    }
+  }
+
 
   return (
     <div className="app">
@@ -45,25 +64,31 @@ function App() {
         <h5 className="app__filterTitle">Filter by Alliances</h5>
         <div className="app__filters">
           <div className="app__filter">
-            <input name="Oneworld" type="checkbox" />
-            <label for="Oneworld">Oneworld</label>
+            <input value="OW" name="Oneworld" onChange={handleChange} type="checkbox" />
+            <label htmlFor="Oneworld">Oneworld</label>
           </div>
           <div className="app__filter">
-            <input name="Sky Team" type="checkbox" />
-            <label for="Sky Team">Sky Team</label>
+            <input value="ST" name="Sky Team" onChange={handleChange} type="checkbox" />
+            <label htmlFor="Sky Team">Sky Team</label>
           </div>
           <div className="app__filter">
-            <input name="Star Alliance" type="checkbox" />
-            <label for="Star Alliance">Star Alliance</label>
+            <input value="SA" name="Star Alliance" onChange={handleChange} type="checkbox" />
+            <label htmlFor="Star Alliance">Star Alliance</label>
           </div>
         </div>
       </div>
       <div className="app__content">
         <div className="app__grid">
-          {/* {data?.map((airlineData) => {})} */}
-          {data?.map((airlineData) => (
-            <Card key={uuid()} airlineData={airlineData} />
-          ))}
+          {data?.map((airlineData) => {
+            if (checkedValues.length === 0){
+              return (<Card key={uuid()} airlineData={airlineData} />)
+            }else{
+              if(checkedValues.includes(airlineData.alliance)){
+                return (<Card key={uuid()} airlineData={airlineData} />)
+              }
+            }
+            return null;
+          })}
         </div>
       </div>
     </div>
